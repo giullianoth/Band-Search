746b0606c0594fea64f030aeaa3d7c3e7ab600ed
Giulliano Guimarães <llthguimaraes@gmail.com>
2025-09-25 10:05:16 -0300

    refatoração: estrutura HTML

--- assets/js/scripts.js
+++
@@ -1,9 +0,0 @@
-import setViewport from "./theme/viewport.js";
-import darkTheme from "./theme/dark-theme.js";
-import jForm from "./theme/form.js";
-
-setViewport();
-jForm();
-darkTheme();
-
-window.onresize = setViewport;
--- assets/js/theme/error.js
+++
@@ -1,9 +0,0 @@
-const error = (message) => {
-    let errorElement = document.createElement("section");
-    errorElement.className = "bs_main_content_error j_error";
-    errorElement.innerHTML = `<p>${message}</p>`;
-
-    return errorElement;
-}
-
-export default error;
